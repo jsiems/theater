bcdd8f827291d2e2d295fb5e15e9ee869c09be85
josh <joshuasiems@gmail.com>
2017-06-11 22:08:14 +0000

    Set up server to send the html file
    This commit allows the server to be able to send the HTML file.
    
    Soon it will check to see if the user is logged in first.
    
    It will be awesome.

--- server/theater.js
+++ server/theater.js
@@ -1,1 +1,12 @@
-la;sdkjsadf
+//Theater server file
+var path = require('path');
+
+module.exports = {
+	initialize: function(app) {
+		console.log("Initializing Theater...")
+		app.get('/theater', function(req, res){
+			res.sendFile(path.join(__dirname, '../client', 'theater.html'));
+			console.log("get request for theater");
+		});
+	}
+}
